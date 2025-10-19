2cf158610ca752400c21a43e17bd1dad29d0e037
Johan Marquez <johanmarquezzuniga460@gmail.com>
2025-10-19 17:32:12 -0500

    Agregando interfaz de fragment game y animacion al cuadro

--- Parcial/app/src/main/java/com/example/juegocolor/GameFragment.kt
+++ Parcial/app/src/main/java/com/example/juegocolor/GameFragment.kt
@@ -5,14 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
 
 
 class GameFragment : Fragment(R.layout.fragment_game) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Encuentra la vista a la que quieres aplicar la animación
+        val view = view.findViewById<View>(R.id.cntn_color)
+        //carga las animaciones usamos require contexto por que es el contexto del fragment
+        val fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
+        val rotate = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate)
 
+// Combinar ambas animaciones
+        val animationSet = AnimationSet(true).apply {
+            addAnimation(fadeIn)
+            addAnimation(rotate)
+        }
+            //inicia la animacion en la vista
+        view.startAnimation(animationSet)
 
 
 
     }
+
 }
