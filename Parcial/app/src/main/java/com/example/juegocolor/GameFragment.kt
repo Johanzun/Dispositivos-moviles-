aa78630e0e6c363df5d244dd9547e8fc72051ced
Johan Marquez <johanmarquezzuniga460@gmail.com>
2025-10-19 17:53:09 -0500

    Agregando botones y gameviewmodel

--- Parcial/app/src/main/java/com/example/juegocolor/GameFragment.kt
+++ Parcial/app/src/main/java/com/example/juegocolor/GameFragment.kt
@@ -7,18 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationSet
 import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import com.example.juegocolor.viewmodel.GameViewModel
+import kotlin.getValue
 
 
 class GameFragment : Fragment(R.layout.fragment_game) {
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
         // Encuentra la vista a la que quieres aplicar la animación
         val view = view.findViewById<View>(R.id.cntn_color)
         //carga las animaciones usamos require contexto por que es el contexto del fragment
         val fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
         val rotate = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate)
 
-// Combinar ambas animaciones
+        // Combinar ambas animaciones
         val animationSet = AnimationSet(true).apply {
             addAnimation(fadeIn)
             addAnimation(rotate)
---
+++ Parcial/app/src/main/java/com/example/juegocolor/viewmodel/GameViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.juegocolor.viewmodel
+
+class GameViewModel {
+}
