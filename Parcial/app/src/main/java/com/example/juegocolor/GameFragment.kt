0c6b5efbad6a9ffb32660d9487facae23253403a
Johan Marquez <johanmarquezzuniga460@gmail.com>
2025-10-19 16:00:06 -0500

    Agregando fragments

---
+++ Parcial/app/src/main/java/com/example/juegocolor/GameFragment.kt
@@ -0,0 +1,18 @@
+package com.example.juegocolor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class GameFragment : Fragment(R.layout.fragment_game) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+    }
+}
